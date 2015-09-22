9b07f03a291ac8114bb6a06c110202b8c626103f
Daniel Yee <daniel.yee@sbmedia.sg>
2015-09-22 14:57:04 +0800

    For Testing

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult LoginOTP()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        public ActionResult PasswordRecovery()
+        {
+            return View();
+        }
+
+        public ActionResult ItemDetailMaintenance()
+        {
+            return View();
+        }
+
+        public ActionResult FormAlert()
+        {
+            return View();
+        }
+	}
+}
